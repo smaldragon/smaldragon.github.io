df757d7de232ec65dbd4a20b68663b0852e640ae
smal <6433336+smaldragon@users.noreply.github.com>
2023-01-21 23:06:50 +0000

    pettyness

---
+++ script.js
@@ -0,0 +1,7 @@
+var incident = new Date(2023, 0, 22); //YYYY, MM, DD //month counts from 00 i.e may = 04 not 05
+var today = Date.now();
+
+var difference = Math.round((today - incident)/(1000*60*60*24));
+document.getElementById('days').innerHTML = difference;
+//document.querySelector('#last-incident').innerHTML = incident; 
+// Use the above line to show last incident. Need to code the formatting first
